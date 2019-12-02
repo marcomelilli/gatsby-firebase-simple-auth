50346340a99abf864db267dcdc799d196f5663be
Marco Melilli <marcomelilli92@hotmail.it>
2019-12-02 10:00:30 +0100

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ const Header = () => (
     <div class="items-center w-1/2 text-white mr-6 text-center">
       <span class="font-semibold text-xl">Gatsby Firebase Auth</span>
     </div>
-    <div class="w-1/2 mt-2 text-sm flex flex-row-reverse">
+    <div class="w-1/2 mt-1 text-md flex flex-row-reverse">
       <Link to="/app/profile">
         <a class="block text-white hover:text-grey-500 mr-4">
           Profile
