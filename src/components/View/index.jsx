0ffb7d7613266371000dc9f08867c7d5675eba5a
Marco Melilli <marcomelilli92@hotmail.it>
2019-12-02 09:59:11 +0100

    Update index.jsx

--- src/components/View/index.jsx
+++ src/components/View/index.jsx
@@ -5,7 +5,7 @@ import styles from "./view.module.css"
 const View = ({ title, children }) => (
     <div className="w-full bg-gray-100 rounded px-8 pt-6 pb-8 mb-4">
       <div className="w-1/3"></div>
-      <div className="bg-white shadow-md rounded px-4 pt-6 pb-8 mb-4 md:mx-24 lg:mx-32">
+      <div className="bg-white shadow-md rounded px-4 pt-6 pb-8 mb-4 md:mx-24 lg:mx-64">
         <h1 className="text-lg font-bold">{title}</h1>
 
         <div className="my-8">
